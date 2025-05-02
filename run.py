f5a5f2d840151af2a954cbe04a1304cc3c2a48ff
rslaporte <rafanex@gmail.com>
2025-05-02 04:00:45 -0300

    Adding logic to retry the scrapper until 3 times in case of failure

--- run.py
+++ run.py
@@ -8,30 +8,40 @@ from scraper import Scraper  # Importa a classe Scraeper do seu módulo
 def main():    
     scraper = Scraper(r'C:\Users\Rafael\Desktop\codes\mtg_scrapper\data\cards_urza-s_saga.csv')
 
+    max_retries = 3
     max_executions = math.ceil(scraper.df_size / scraper.batch_size) 
-    executions = 0
+    executions = 0   
 
     def job():        
+        retry_count = 1
+        nonlocal executions
+        success = False
+
+        print(f"Execução {executions + 1} iniciada...")
         with open("execution.log", "a") as f:
             f.write("##################################################################################\n")
             f.write(f"EXECUTANDO EM: {datetime.now()}\n")
 
-        nonlocal executions
-        
-        print(f"Execução {executions + 1} iniciada...")
-        try:
-            asyncio.run(scraper.run(executions)) 
-        except NameError:
-            with open("execution.log", "a") as f:
-                f.write(f"{datetime.now()} ERRO: {NameError}\n")
-                   
+        while not success and retry_count <= max_retries:              
+            try:
+                asyncio.run(scraper.run(executions)) 
+                success = True
+
+            except Exception as e:
+                retry_count += 1
+
+                with open("execution.log", "a") as f:
+                    f.write(f"{datetime.now()} - ERRO NA EXECUCAO {executions + 1}: {str(e)}\n")
+
+                if retry_count <= max_retries:                    
+                    time.sleep(180)                   
 
         executions += 1
         if executions >= max_executions:
             print("Execuções concluídas. Encerrando agendador.")
 
             with open("execution.log", "a") as f:
-                f.write(f"{datetime.now()} ERRO: {NameError}\n")
+                f.write(f"EXECUCAO DO SCRIPT CONCLUIDA.\n")
 
             return schedule.CancelJob 
         
